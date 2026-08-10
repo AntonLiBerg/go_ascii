f9768cff678daee9927e2c23501355874ef689c9
Medo <antonliberg93@gmail.com>
2026-08-10 22:56:42 +0200

    .

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,5 +1,7 @@
 package helpers
 
+import "slices"
+
 func Filter[T any](values []T, keep func(T) bool) []T {
 	result := make([]T, 0, len(values))
 	for _, value := range values {
@@ -34,4 +36,21 @@ func Transform[T any](values []T, transform func(T) T) []T {
 		result = append(result, transform(value))
 	}
 	return result
+}
+
+func IsAllS1InS2(slice1 []string, slice2 []string) bool {
+	return All(slice1, func(value string) bool {
+		return slices.Contains(slice2, value)
+	})
+}
+
+func IsUnique(values []string) bool {
+	seen := make(map[string]struct{}, len(values))
+	for _, value := range values {
+		if _, exists := seen[value]; exists {
+			return false
+		}
+		seen[value] = struct{}{}
+	}
+	return true
 }
--- internal/scenario/ui_validate.go
+++ internal/scenario/ui_validate.go
@@ -2,27 +2,10 @@ package scenario
 
 import (
 	"fmt"
-	"go_ascii/internal/helpers"
+	hlp "go_ascii/internal/helpers"
 	"slices"
 	"strings"
 )
-
-func isAllS1InS2(slice1 []string, slice2 []string) bool {
-	return helpers.All(slice1, func(value string) bool {
-		return slices.Contains(slice2, value)
-	})
-}
-
-func isUnique(values []string) bool {
-	seen := make(map[string]struct{}, len(values))
-	for _, value := range values {
-		if _, exists := seen[value]; exists {
-			return false
-		}
-		seen[value] = struct{}{}
-	}
-	return true
-}
 
 func isUiFileValid(lines []string) error {
 	if len(lines) <= 1 {
@@ -46,27 +29,27 @@ func isUiFileValid(lines []string) error {
 	}
 
 	uiNonLayoutSections := lines[layoutEnd:]
-	sectionHeaders := helpers.Filter(uiNonLayoutSections, isUISectionHeader)
-	sectionHeaders = helpers.Transform(sectionHeaders, func(header string) string {
+	sectionHeaders := hlp.Filter(uiNonLayoutSections, isUISectionHeader)
+	sectionHeaders = hlp.Transform(sectionHeaders, func(header string) string {
 		return strings.TrimSpace(strings.TrimPrefix(header, SectionDivider))
 	})
 
 	if !slices.Contains(uiLayout, "room") {
 		return fmt.Errorf("layout does not contain required keyword room")
 	}
-	if !isUnique(uiLayout) {
+	if !hlp.IsUnique(uiLayout) {
 		return fmt.Errorf("duplicate headers in layout!")
 	}
-	if !isUnique(sectionHeaders) {
+	if !hlp.IsUnique(sectionHeaders) {
 		return fmt.Errorf("duplicate headers in UI file!")
 	}
-	if !isAllS1InS2(sectionHeaders, uiLayout) {
+	if !hlp.IsAllS1InS2(sectionHeaders, uiLayout) {
 		return fmt.Errorf("some sections are not mentioned in the layout!")
 	}
-	uiSectionsInLayout := helpers.Filter(uiLayout, func(name string) bool {
+	uiSectionsInLayout := hlp.Filter(uiLayout, func(name string) bool {
 		return name != "room"
 	})
-	if !isAllS1InS2(uiSectionsInLayout, sectionHeaders) {
+	if !hlp.IsAllS1InS2(uiSectionsInLayout, sectionHeaders) {
 		return fmt.Errorf("some sections mentioned in layout do not exist in the UI file")
 	}
 	return nil
